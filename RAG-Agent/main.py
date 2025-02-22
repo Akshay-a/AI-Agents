5f2929925c9667f421600d52f14d39a0d7594240
Akshay-a <apsingiakshay46@gmail.com>
2025-02-22 22:11:23 +0530

    chroma db testing with a sample pdf

--- RAG-Agent/main.py
+++ RAG-Agent/main.py
@@ -1,143 +1,112 @@
 import os
-import streamlit as st
+import logging
+
 
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_chroma import Chroma
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters.sentence_transformers import SentenceTransformersTokenTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough
+from langchain_core.runnables import RunnablePassthrough,RunnableParallel
 from langchain_ollama import OllamaLLM
-from sentence_transformers import SentenceTransformer
 from langchain_huggingface import HuggingFaceEmbeddings
 
-# Initialize embedding model
-#embedding_model = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-embedding_model = HuggingFaceEmbeddings(model_name="dunzhang/stella_en_1.5B_v5")
-#SentenceTransformer('all-MiniLM-L6-v2')
-# Initialize KB database
-db = Chroma(collection_name="cp_database",
-            embedding_function=embedding_model,
-            persist_directory='./db_knowledge_base')
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-def format_docs(docs):
+def load_rag_chain():
+    logger.info("Running RAG chain...")
+    retriever = db.as_retriever(search_type="similarity", search_kwargs={'k': 5})
+    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+    chat_model = OllamaLLM(model="qwen2.5:7b", base_url="http://localhost:11434", temperature=0.6)
+    logger.info("Chat model initialized.")
+    output_parser = StrOutputParser()
+    logger.info("Output parser initialized.")
+    return {"context": retriever | format_docs, "question": RunnablePassthrough()} | prompt_template | chat_model | output_parser
+
+
+PROMPT_TEMPLATE = """
+        You are a highly knowledgeable assistant specializing in computer science. 
+        Answer the question based only on the following context:
+        {context}
+
+        Answer the question based on the above context:
+        {question}
+
+        Use the provided context to answer the user's question accurately and concisely.
+        Don't justify your answers.
+        Don't give information not mentioned in the CONTEXT INFORMATION.
+        Do not say "according to the context" or "mentioned in the context" or similar.
+        """
+
+# Initialize embedding model
+embedding_model = HuggingFaceEmbeddings(model_name="dunzhang/stella_en_1.5B_v5")
+# Initialize KB database
+db = Chroma(collection_name="cp_database", embedding_function=embedding_model, persist_directory='./db_knowledge_base')
+
+def format_docs(docs) -> list:
     return "\n\n".join(doc.page_content for doc in docs)
 
-def add_to_db(uploaded_files):
-    # Check if files are uploaded
-    if not uploaded_files:
-        st.error("No files uploaded!")
-        return
-
-    for uploaded_file in uploaded_files:
-        # Save the uploaded file to a temporary path
-        temp_file_path = os.path.join("./temp", uploaded_file.name)
-        os.makedirs(os.path.dirname(temp_file_path), exist_ok=True)
-
-        with open(temp_file_path, "wb") as temp_file:
-            temp_file.write(uploaded_file.getbuffer())
-
-        # Load the file using PyPDFLoader
-        loader = PyPDFLoader(temp_file_path,extract_images = False)
-        data = loader.load()
-
-        # Store metadata and content
-        doc_metadata = [data[i].metadata for i in range(len(data))]
-        doc_content = [data[i].page_content for i in range(len(data))]
-
-        # Split documents into smaller chunks
-        st_text_splitter = SentenceTransformersTokenTextSplitter(
-            model_name="sentence-transformers/all-mpnet-base-v2",
-            chunk_size=100,
-            chunk_overlap=50
-        )
-        st_chunks = st_text_splitter.create_documents(doc_content, doc_metadata)
-
-        # Add chunks to database
-        #db.add_documents(st_chunks)
-        db.add_documents(data)
-
-        # Remove the temporary file after processing
-        os.remove(temp_file_path)
-
 def run_rag_chain(query):
-    # Create a Retriever Object and apply Similarity Search
-    retriever = db.as_retriever(search_type="similarity", search_kwargs={'k': 5})
-
-    # Initialize a Chat Prompt Template
-    PROMPT_TEMPLATE = """
-    You are a highly knowledgeable assistant specializing in computer science. 
-    Answer the question based only on the following context:
-    {context}
-
-    Answer the question based on the above context:
-    {question}
-
-    Use the provided context to answer the user's question accurately and concisely.
-    Don't justify your answers.
-    Don't give information not mentioned in the CONTEXT INFORMATION.
-    Do not say "according to the context" or "mentioned in the context" or similar.
-    """
-
-    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
-
-    # Initialize a Generator (i.e. Chat Model)
-    chat_model = OllamaLLM(
-        model="qwen2.5:7b",
-        base_url="http://localhost:11434",  # Assuming Ollama is running locally on this port
-        temperature=1
-    )
-
-    # Initialize a Output Parser
-    output_parser = StrOutputParser()
-
-    # RAG Chain
-    rag_chain = {"context": retriever | format_docs, "question": RunnablePassthrough()} | prompt_template | chat_model | output_parser
-
-    # Invoke the Chain
-    response = rag_chain.invoke(query)
-
-    return response
-
+    try:
+        rag_chain = load_rag_chain()
+        logger.info("RAG chain created.")
+        response = rag_chain.invoke(query)
+        logger.info(f"Query processed successfully.response: {response}")
+        return response
+    except Exception as e:
+        logger.error(f"An error occurred: {str(e)}")
+        return None
 
 def main():
-    st.set_page_config(page_title="Query", page_icon=":microscope:")
-    st.header("KB Retrieval System")
+    try:
 
-    query = st.text_area(
-        ":bulb: Enter your query about the document:",
-        placeholder="e.g., What is the info on document?"
-    )
+        loader = PyPDFLoader('./RAG-Agent/temp/SOP.pdf', extract_images=False)
+        data = loader.load()
 
-    if st.button("Submit"):
-        if not query:
-            st.warning("Please ask a question")
+        doc_metadata = [data[i].metadata for i in range(len(data))]
+        doc_content = [data[i].page_content for i in range(len(data))]
+        print("doc content is ")
+        print(doc_content)
+        st_text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=500,
+            chunk_overlap=100,
+            separators=["\n \n", "\n", ".", " "])
+        st_chunks = st_text_splitter.create_documents(doc_content, doc_metadata)
         
-        else:
-            with st.spinner("Thinking..."):
-                result = run_rag_chain(query=query)
-                st.write(result)
-
-    
-    with st.sidebar:
-        st.markdown("---")
-        pdf_docs = st.file_uploader("Upload PDF to the knowledge base",
-                                    type=["pdf"],
-                                    accept_multiple_files=True
-        )
+        print(st_chunks)
+        db.add_documents(st_chunks)
+        print("Processed and added to the database!")
+        #ask the query below now to get the response
+        query="tell me about Goutham kumar"
+        retriever=db.as_retriever(k=5)
+        prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+        chat_model = OllamaLLM(model="qwen2.5:7b", base_url="http://localhost:11434", temperature=0.6)
+        logger.info("Chat model initialized.")
+        output_parser = StrOutputParser()
+        logger.info("Output parser initialized.")
         
-        if st.button("Submit & Process"):
-            if not pdf_docs:
-                st.warning("Please upload the file")
 
-            else:
-                with st.spinner("Processing your documents..."):
-                    add_to_db(pdf_docs)
-                    st.success(":file_folder: Documents successfully added to the database!")
+    #  Get the query embedding
+        query_embedding = embedding_model.embed_query(query)
+        print("Query Embedding (first 10 values):", query_embedding[:10])  # Long vector, showing first 10 for brevity
+        print("Query Embedding Length:", len(query_embedding))
 
-    # Sidebar Footer
-    st.sidebar.write("Just a Side Project!!")
-             
+        # Use Chroma's similarity search with scores to get documents and metadata
+        docs_with_scores = db.similarity_search_with_score(query, k=5)
+
+        # Inspect retrieved documents and fetch their embeddings
+        print("\nRetrieved Documents and Embeddings:")
+        print(docs_with_scores)
+
+
+        op = RunnableParallel({"context": retriever | format_docs, "question": RunnablePassthrough()}) | prompt_template | chat_model | output_parser
+        print(f'response fetched from LLM and result is {op.invoke(query)}')
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
 if __name__ == "__main__":
     main()
